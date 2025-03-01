7e4d1bf6798b7b3764c19f45c7a95c60faafa12b
sm-amoled <moktak072@naver.com>
2025-03-01 14:22:39 +0900

    [#1] fix: pacaBlack, White 색상 오류 수정

--- lib/presentation/common/theme/theme_constants.dart
+++ lib/presentation/common/theme/theme_constants.dart
@@ -7,8 +7,8 @@ ThemeData darkTheme = ThemeData(brightness: Brightness.dark, textTheme: pacamara
 extension PacamaraColorTheme on ThemeData {
   Color get pacaAccent300 => brightness == Brightness.light ? const Color(0xFFF3DAD8) : const Color(0xFF37302E);
   Color get pacaAccent500 => const Color(0xFFEF4B3F);
-  Color get pacaBlack => brightness == Brightness.light ? const Color(0xFFF3DAD8) : const Color(0xFF37302E);
-  Color get pacaWhite => brightness == Brightness.light ? const Color(0xFFF0F0F0) : const Color(0xFF151515);
+  Color get pacaBlack => brightness == Brightness.light ? const Color(0xFFF0F0F0) : const Color(0xFF151515);
+  Color get pacaWhite => brightness == Brightness.light ? const Color(0xFF151515) : const Color(0xFFF0F0F0);
   Color get pacaGray200 => brightness == Brightness.light ? const Color(0xFFD8D8D8) : const Color(0xFF363636);
   Color get pacaGray600 => brightness == Brightness.light ? const Color(0xFFA0A0A1) : const Color(0xFF616161);
   Color get pacaGray800 => brightness == Brightness.light ? const Color(0xFF222222) : const Color(0xFFC4C4C4);
